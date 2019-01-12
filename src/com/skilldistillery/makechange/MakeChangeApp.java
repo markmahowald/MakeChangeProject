bb66e31a45cf23d58257523bbe36d6a84aa00635
markmahowald <markmahowald0328@gmail.com>
2019-01-12 14:40:40 -0700

    Code refactored - change & remainder methods combined, all variables now in arrays, README updated, final print will not print any denominations that are not required

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -8,9 +8,13 @@ public class MakeChangeApp {
 		Scanner kb = new Scanner(System.in);
 		MakeChangeDriver reg = new MakeChangeDriver();
 		
-
+/*denomination array holds US denominations in cents (for modulus function to work properly), and change holds the amount of each denomination, in the same order as denomination so that their indexes match.  . 
+ * Layout - Lines 16 -> 17 declare all necessary variables and arrays. Lines 19 -> 33 cover getting the user's inputs, and dealing with (i) not enough money offered, and (ii) exact change offered.  lines 34 on, cover calculating change through a looping for statement to see if any bills/coins of a specific denomination are required. 
+ * 
+ */
+		
 		double price, money, changeDue;
-		int hundred = 0, fifty = 0, twenty = 0, ten = 0, five = 0, one = 0, quarter = 0, dime = 0, nickle = 0, cent = 0, diff = 0;
+		int diff = 0, i = 0,  denomination[] = new int [] {10000, 5000, 2000, 1000, 500, 100, 25, 10, 5, 1}, change[] = new int [10];
 
 		System.out.println("Hello. What is the price of the customer's item?");
 		price = kb.nextDouble();
@@ -21,90 +25,29 @@ public class MakeChangeApp {
 		if (!reg.enough(price, money)) {
 			System.out.println("Tell the customer \"Im sorry, thats not enough money.\" Transaction canelled.");
 			System.exit(0);
-			// TODO - this may not be right - we may want to fix so that the user is
 			// prompted again.
 
-		} else {
-			changeDue = money - price;
-			System.out.println("They need " + changeDue + " in change");
-
-			diff = (int) ((money - price) * 100);
-			while (diff > 1) {
-
-				if (diff >= 10000) {
-					hundred = reg.change(diff, 10000);
-					diff = reg.remainder(diff, 10000);
-				}
-				if (diff >= 5000) {
-					fifty = reg.change(diff, 5000);
-					diff = reg.remainder(diff, 5000);
-				}
-				if (diff >= 2000) {
-					twenty = reg.change(diff, 2000);
-					diff = reg.remainder(diff, 2000);
-				}
-				if (diff >= 1000) {
-					ten = reg.change(diff, 1000);
-					diff = reg.remainder(diff, 1000);
-				}
-				if (diff >= 500) {
-					five = reg.change(diff, 500);
-					diff = reg.remainder(diff, 500);
-				}
-				if (diff >= 100) {
-					one = reg.change(diff, 100);
-					diff = reg.remainder(diff, 100);
-				}
-				if (diff >= 25) {
-					quarter = reg.change(diff, 25);
-					diff = reg.remainder(diff, 25);
-				}
-				if (diff >= 10) {
-					dime = reg.change(diff, 10);
-					diff = reg.remainder(diff, 10);
-				}
-				if (diff >= 5) {
-					nickle = reg.change(diff, 5);
-					diff = reg.remainder(diff, 5);
-				}
-				if (diff >= 1) {
-					cent = reg.change(diff, 1);
-					diff = reg.remainder(diff, 1);
-				}
-
-			}
-			System.out.println("The customer's change is: ");
-			System.out.println(hundred + "hundreds");
-			System.out.println(fifty + "fifties");
-			System.out.println(twenty + "twenties");
-			System.out.println(ten + "tens");
-			System.out.println(five + "fives");
-			System.out.println(one + "ones");
-			System.out.println(quarter + "quarters");
-			System.out.println(dime + "dimes");
-			System.out.println(nickle + "nickles");
-			System.out.println(cent + "pennies");
-
+		} else if (price == money) {
+			System.out.println("Exact change! Transaction complete");
+			System.exit(0);	
 		}
-//			if (diff>=0.25) {
-	}
-	
-	
-	// TODO - figure this part out....
+		else {
+			changeDue = money - price;
+			
+			System.out.print("They need ");
+			System.out.printf("%.2f", changeDue);
+			System.out.println(" back");
+			
+			diff = (int) (Math.round((money - price) * 100));
+			
+			for(i = 0; i<10; i++) {
+			
+				if (diff >= denomination[i]) {
+					diff = reg.change(diff, denomination, change, i);
+				}
+			}
+			reg.printChange(change );
+			kb.close();
+		}
+	}	
 }
-
-// call cashregister
-
-//Hint: Mod operator
-//
-//User Story #1
-//The user is prompted asking for the price of the item.
-//
-//User Story #2
-//The user is then prompted asking how much money was tendered by the customer.
-//
-//User Story #3
-//Display an appropriate message if the customer provided too little money or the exact amount.
-//
-//User Story #4
-//If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
--- src/com/skilldistillery/makechange/MakeChangeDriver.java
+++ src/com/skilldistillery/makechange/MakeChangeDriver.java
@@ -10,15 +10,49 @@ public class MakeChangeDriver {
 
 	}
 
-	public int change(int diff, int size) {
-		int bills = ((int) diff) / size;
-		return bills;
-
-	}
-
-	public int remainder(int diff, int size) {
-		int remainder = ((int) diff) % size;
+	public int change(int diff, int [] denomination, int []change, int i) {
+		int bills = (diff) / denomination[i];
+		change[i] =  bills;
+		
+		int remainder = (diff) % denomination[i];
 		return remainder;
+		
+	}
+	public void  printChange(int[] change){
+		
+		System.out.println("The customer's change is: ");
+	
+	if(change[0]>0) {
+		System.out.println(change[0] + " hundreds");
+	}
+	if(change[1]>0) {
+		System.out.println(change[1] + " fifties");
+	}
+	if(change[2]>0) {
+		System.out.println(change[2] + " twenties");
+	}
+	if(change[3]>0) {
+		System.out.println(change[3] + " tens");
+	}
+	if(change[4]>0) {
+		System.out.println(change[4] + " fives");
+	}
+	if(change[5]>0) {
+		System.out.println(change[5] + " ones");
+	}
+	if(change[6]>0) {
+		System.out.println(change[6] + " quarters");
+	}
+	if(change[7]>0) {
+		System.out.println(change[7] + " dimes");
+	}
+	if(change[8]>0) {
+		System.out.println(change[8] + " nickles");
+	}
+	if(change[9]>0) {
+		System.out.println(change[9] + " pennies");
+	}
+	
 	}
 
 }
